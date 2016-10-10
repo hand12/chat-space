aa575cf5310ed1562803d2747cf7236c64e95ee3
hand12 <hand12.k@gmail.com>
2016-10-11 01:12:59 +0900

    add setting code to dont generate

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,11 @@ module ChatSpace
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.test_framework       false
+      g.helper               false
+      g.stylesheets          false
+      g.javascripts          false
+    end
   end
 end
