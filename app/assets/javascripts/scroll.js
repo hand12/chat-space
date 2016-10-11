7f8455c2d7741223676269fda4be0d620f9b05dd
hand12 <hand12.k@gmail.com>
2016-10-11 23:49:08 +0900

    turining scroll function

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,10 +1,14 @@
 $(document).on('turbolinks:load', function(){
   $('.chat-footer__submit').on("click", function(){
+    scroll();
+    $('#js-flash').fadeIn().delay(3000).fadeOut(500);
+  });
+
+  scroll();
+
+  function scroll(){
     var target = $('.chat-main');
     target.animate({scrollTop: $('.chat-body').height()}, 500);
-
-    $('#js-flash').fadeIn().delay(3000).fadeOut(500);
-
-  });
+  }
 })
 
