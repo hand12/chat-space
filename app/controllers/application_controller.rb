4b3e0dbb5ca082e7ec4cc7649b24c4b5f6c4d9a6
hand12 <hand12.k@gmail.com>
2016-10-16 17:25:53 +0900

    modify some errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,6 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    root_path
+    new_group_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resources :messages
-  resources :groups
+  resources :messages, only: [:index, :create]
+  resources :groups, only: [:index, :new, :create, :update, :edit]
+  resources :users, only: [:index]
 end
